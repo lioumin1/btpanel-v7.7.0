b1372e0616b8d78750e685bdfce9939293d789b3
jockerL <48760229+lioumin1@users.noreply.github.com>
2022-12-09 14:48:08 +0800

    Update install_panel.sh

--- install/install_panel.sh
+++ install/install_panel.sh
@@ -221,7 +221,7 @@ Install_RPM_Pack(){
 	#yumBaseUrl=$(cat /etc/yum.repos.d/CentOS-Base.repo|grep baseurl=http|cut -d '=' -f 2|cut -d '$' -f 1|head -n 1)
 	#[ "${yumBaseUrl}" ] && checkYumRepo=$(curl --connect-timeout 5 --head -s -o /dev/null -w %{http_code} ${yumBaseUrl})	
 	#if [ "${checkYumRepo}" != "200" ] && [ "${SYS_TYPE}" ];then
-	#	curl -Ss --connect-timeout 3 -m 60 https://raw.githubusercontent.com/8838/btpanel-v7.7.0/main/install/yumRepo_select.sh|bash
+	#	curl -Ss --connect-timeout 3 -m 60 https://raw.githubusercontent.com/lioumin1/btpanel-v7.7.0/main/install/yumRepo_select.sh|bash
 	#fi
 	
 	#尝试同步时间(从bt.cn)
@@ -355,7 +355,7 @@ Get_Versions(){
 	fi
 }
 Install_Python_Lib(){
-	curl -Ss --connect-timeout 3 -m 60 https://raw.githubusercontent.com/8838/btpanel-v7.7.0/main/install/pip_select.sh|bash
+	curl -Ss --connect-timeout 3 -m 60 https://raw.githubusercontent.com/lioumin1/btpanel-v7.7.0/main/install/pip_select.sh|bash
 	pyenv_path="/www/server/panel"
 	if [ -f $pyenv_path/pyenv/bin/python ];then
 	 	is_ssl=$($python_bin -c "import ssl" 2>&1|grep cannot)
